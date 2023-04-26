6745b6a7d33fe81f2549aa0b0bb7a02131c7d4a8
josue.0 <josueajc@gmail.com>
2023-04-26 13:38:46 -0500

    compilation fixes and prod deployment

--- functions/src/server/router01.ts
+++ functions/src/server/router01.ts
@@ -1,5 +1,4 @@
 import * as express from "express";
-import { createNewEvent } from "./logic/rally-event-01";
 import { getFirebaseAuthMiddleware } from "./util/express-firebase-middleware";
 import { logRequest } from "./util/log-request-middleware";
 import { logAndAnswer } from "./util/utils";
@@ -16,44 +15,5 @@ export const routerV1 = express
   .use(logRequest)
 
 
-  // endpoints code:
-
-
-  .post('/p/createEvent', async function (req, res) {
-    const userId = res.locals.userId as string;
-    const rawBody = req.body;
-
-    try {
-      const { docId: eventId, eventData } = await createNewEvent({
-        name: String(rawBody.name),
-        start_time: Number(rawBody.start_time),
-        end_time: Number(rawBody.end_time),
-        upload_deadline: Number(rawBody.upload_deadline),
-        place: String(rawBody.place),
-        lat: rawBody.lat == null ? null : Number(rawBody.lat),
-        lon: rawBody.lon == null ? null : Number(rawBody.lon),
-        category: String(rawBody.category),
-        notes: String(rawBody.notes),
-        distance: Number(rawBody.distance),
-        speed: Number(rawBody.speed),
-        place_id: rawBody.place_id == null ? null : String(rawBody.place_id),
-        zip_code: rawBody.zip_code == null ? null : String(rawBody.zip_code),
-        city: rawBody.city == null ? null : String(rawBody.city),
-        address_line_2: rawBody.address_line_2 == null ? null : String(rawBody.address_line_2),
-      }, userId);
-
-
-      logAndAnswer(res, {
-        status: "OK",
-        docId: eventId,
-        eventData,
-      });
-    } catch (e) {
-      logAndAnswer(res, {
-        status: "ERROR",
-        message: (e as any).message,
-      });
-    }
-  })
   ;
 
